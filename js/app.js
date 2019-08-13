c047d49ef3784ad0dae2218d6e86beff5ee20393
Anton Suhorada <fpscinema1@gmail.com>
2019-08-13 14:53:00 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector('.diss'),
+    intensity: 0.1,
+    image1: '../source/car1.webp',
+    image2: '../source/car2.webp',
+    displacementImage: '../source/height.png'
+});
